89435a742a88d8c46e326c9a6d38da0d74c20f23
Hasmik151515 <hasmikvardazaryan.ait@gmail.com>
2025-08-02 20:05:18 +0400

    finish maxpeople

--- src/components/MaxPeople.tsx
+++ src/components/MaxPeople.tsx
@@ -14,7 +14,7 @@ const MaxPeople: React.FC = () => {
   };
 
   return (
-    <div className="absolute top-[520px] left-[120px]">
+    <div className="absolute top-[550px] left-[120px]">
       <p className="text-[16px] font-medium">Max people count</p>
       <div className=" relative top-[20px] left-[0px] h-[40px] w-[40px] bg-gray-200 rounded-full flex justify-center items-center font-medium cursor-pointer" onClick={handleMinus}>-</div>
       <input className="relative top-[-25px] left-[60px] w-[70px] p-3 rounded-[10px] border  border-gray-200 font-medium" type="number" value={peopleCount} readOnly />
